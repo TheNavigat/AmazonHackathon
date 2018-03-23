0af2703ee4250326c55627abbda90d7ef2a4b57f
Mostafa Shehab <mostafa.shehab4858@gmail.com>
2018-03-23 18:51:31 +0200

    Adding libs and mic change

--- dev/vivavoce/urls.py
+++ dev/vivavoce/urls.py
@@ -4,8 +4,7 @@ from . import views
 app_name = 'vivavoce'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('start/', views.start, name='start'),
+    path('start/<int:question_id>/', views.start, name='start'),
     path('record/', views.RecordView.as_view(), name='record'),
     path('upload/', views.upload, name='upload'),
 ]
-
