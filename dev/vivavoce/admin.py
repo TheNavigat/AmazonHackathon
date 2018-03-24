57999acefb530f0dfa20f26630ff00bdce66d6b7
Karim ElGhandour <kghandour@live.com>
2018-03-24 04:26:43 +0200

    malhash lazma

--- dev/vivavoce/admin.py
+++ dev/vivavoce/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Question
+from .models import Question, TranscribeJob
 
 
 admin.site.register(Question)
+admin.site.register(TranscribeJob)
+
 # Register your models here.
